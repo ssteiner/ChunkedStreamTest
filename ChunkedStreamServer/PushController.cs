e62a48265f3aab8bede92d173b49550867692a45
ssteiner <steiner@hotmail.com>
2014-01-07 11:02:10 +0100

    added secondary PollData method that takes a request parameter

--- ChunkedStreamServer/PushController.cs
+++ ChunkedStreamServer/PushController.cs
@@ -14,6 +14,18 @@ namespace ChunkedStreamServer
     {
         int run = 0;
 
+
+        [HttpGet]
+        [Route("PollData")]
+        public HttpResponseMessage PollData(string sessionId)
+        {
+            log("Polling requst recevied from " + sessionId, 4);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+            //response.Headers.TransferEncodingChunked = true;
+            response.Content = new PushStreamContent((streamContent, content, context) => writePushData(streamContent, content, context), "application/json");
+            return response;
+        }
+
         [HttpGet]
         [Route("PollData")]
         public HttpResponseMessage PollData()
@@ -34,8 +46,21 @@ namespace ChunkedStreamServer
                 await Task.Delay(1000);
                 var str = "{ \"data\" : \"" + run + "\"}";
                 var buffer = UTF8Encoding.UTF8.GetBytes(str);
-                log(str, 4);
-                await sw.WriteLineAsync(str);
+                log("sending " + str, 4);
+                try
+                {
+                    await sw.WriteLineAsync(str);
+                }
+                catch (IOException i)
+                {
+                    log("Writing to stream failed: " + i.Message, 2);
+                    break;
+                }
+                catch (ObjectDisposedException o)
+                {
+                    log("Unable to write to stream, object disposed: " + o.Message, 2);
+                    break;
+                }
             }
         }
 
